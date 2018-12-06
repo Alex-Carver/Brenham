ee21ae30eab8fff1c036983e77795552bf0dabc3
Alex <Alex-Carver@.noreply.github.com>
2018-12-06 09:51:40 -0600

    Home Page

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$('.carousel').carousel({
+  interval: 5000
+})
+
+$(function() {
+  $('.inactiveUntilOnLoad').removeClass('inactiveUntilOnLoad');
+});
+
+
+
+$(document).ready(function() {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $('#nav_bar').addClass('fixed-top');
+    }
+    if ($(window).scrollTop() > 100) {
+      $('#nav_bar').removeClass('fixed-bottom');
+    }
+     if ($(window).scrollTop() < 100) {
+      $('#nav_bar').removeClass('fixed-top');
+    }
+      if ($(window).scrollTop() < 100) {
+      $('#nav_bar').addClass('fixed-bottom');
+    }
+  });
+});
+
